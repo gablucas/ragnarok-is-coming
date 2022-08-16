d49835ee199a4f6966b5b9dfcd2ff30586b4c321
Gabthedeveloper <gabthedeveloper@gmail.com>
2022-08-16 14:50:49 -0300

    Ragnarok is coming

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import Countdown from "./countdown.js";
-const gowRagnarok = new Countdown('09 November 2022 00:00:00')
+const gowRagnarok = new Countdown('09 November 2022 00:00:00 GMT-0300')
 
 const elementTime = document.querySelectorAll('span');
 
@@ -8,4 +8,4 @@ setInterval(() => {
     elementTime[1].innerText = gowRagnarok.total.hours;
     elementTime[2].innerText = gowRagnarok.total.minutes;
     elementTime[3].innerText = gowRagnarok.total.seconds;
-})
+}, 1000)
