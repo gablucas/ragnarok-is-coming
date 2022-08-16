e5bd7e15f193e445df4c8500f41eed1f6d397cbf
Gabriel Lucas <gabthedeveloper@gmail.com>
2022-08-15 22:24:39 -0300

    Ragnarok is comming

---
+++ script.js
@@ -0,0 +1,11 @@
+import Countdown from "./countdown.js";
+const gowRagnarok = new Countdown('09 November 2022 00:00:00')
+
+const elementTime = document.querySelectorAll('span');
+
+setInterval(() => {
+    elementTime[0].innerText = gowRagnarok.total.days;
+    elementTime[1].innerText = gowRagnarok.total.hours;
+    elementTime[2].innerText = gowRagnarok.total.minutes;
+    elementTime[3].innerText = gowRagnarok.total.seconds;
+})
